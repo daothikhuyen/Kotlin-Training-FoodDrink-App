b682fda162b99b43cad5e477271b47846e4f7206
daothikhuyen <daothikhuyen30@gmail.com>
2026-04-16 11:13:26 +0700

    feat(recycleView) Làm CRUD cho danh sách recycleView

--- app/src/main/java/com/example/exerciseapplication/ui/home/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/exerciseapplication/ui/home/adapter/HomeAdapter.kt
@@ -6,16 +6,15 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.exerciseapplication.ui.home.HomeActivity
 import com.example.exerciseapplication.ui.home.fragment.ItemFragment
 
-class HomeAdapter (activity: AppCompatActivity)
-    : FragmentStateAdapter(activity) {
+class HomeAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
 
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            HomeActivity.TAG_FOOD -> ItemFragment.newInstance(HomeActivity.TAG_FOOD)
-            HomeActivity.TAG_DRINK -> ItemFragment.newInstance(HomeActivity.TAG_DRINK)
-            else -> ItemFragment.newInstance(HomeActivity.TAG_FOOD)
+        return if (position == 0) {
+            ItemFragment.newInstance(true)
+        } else {
+            ItemFragment.newInstance(false)
         }
     }
 }
--- app/src/main/java/com/example/exerciseapplication/ui/home/fragment/ItemFragment.kt
+++ app/src/main/java/com/example/exerciseapplication/ui/home/fragment/ItemFragment.kt
@@ -1,13 +1,21 @@
 package com.example.exerciseapplication.ui.home.fragment
 
+import android.app.AlertDialog
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exerciseapplication.R
 import com.example.exerciseapplication.databinding.FragmentItemBinding
+import com.example.exerciseapplication.model.MenuItem
+import com.example.exerciseapplication.ui.home.adapter.RecycleViewAdapter
 import com.example.exerciseapplication.ui.home.viewmodel.HomeViewModel
 import com.example.exerciseapplication.utils.setBorderColor
 
@@ -16,7 +24,7 @@ class ItemFragment : Fragment() {
     private var _binding: FragmentItemBinding? = null
     private val binding get() = _binding!!
     private val viewModel: HomeViewModel by activityViewModels()
-    private var type: Int = 0
+    private var isFood: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,33 +32,99 @@ class ItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentItemBinding.inflate(inflater, container, false)
-        type = arguments?.getInt(ARG_TYPE) ?: 0
+        isFood = arguments?.getBoolean(ARG_TYPE) ?: false
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (type == 0) {
-            binding.tvHeader.setBorderColor(R.color.lightRed, R.color.redBorder, 6)
-            binding.tvTitle.setBorderColor(borderColor = R.color.lightRed, borderSize = 8)
+        val adapter = RecycleViewAdapter()
+        binding.rvItem.adapter = adapter
+        binding.rvItem.layoutManager = LinearLayoutManager(context)
 
-            viewModel.food.observe(viewLifecycleOwner) {
-                binding.tvHeader.text = getString(R.string.listFood)
-                binding.tvTitle.text = getString(it.name)
+        if (isFood) {
+            binding.tvHeader.setBorderColor(R.color.lightRed, R.color.redBorder, 6)
+            binding.tvHeader.text = getString(R.string.listFood)
+
+            viewModel.food.observe(viewLifecycleOwner) { list ->
+                list?.let {
+                    adapter.setData(it)
+                }
             }
 
         } else {
             binding.tvHeader.setBorderColor(R.color.lightGreen, R.color.greenMain)
-            binding.tvTitle.setBorderColor(
-                borderSize = 8, borderColor = R.color.greenMain,
-            )
+            binding.tvHeader.text = getString(R.string.listDrink)
 
-            // observe: lắng nghe sự thay đổi của dữ liệu/ không observer dữ liệu chỉ lắng nghe 1 lần
-            viewModel.drink.observe(viewLifecycleOwner) {
-                binding.tvHeader.text = getString(R.string.listDrink)
-                binding.tvTitle.text = getString(it.name)
+            // observe: lắng nghe sự thay đổi của dữ liệu
+            viewModel.drink.observe(viewLifecycleOwner) { list ->
+                list?.let {
+                    adapter.setData(it)
+                }
             }
+        }
+
+        adapter.onDeleteItem = { item: MenuItem ->
+            viewModel.deleteItem(isFood, item)
+        }
+
+        adapter.onUpdateItem = { item: MenuItem ->
+            openAddDialog(item)
+        }
+    }
+
+    fun openAddDialog(item: MenuItem? = null) {
+        if(item == null) {
+            showAddDialog { name, price ->
+                viewModel.addItem(isFood, name, price)
+            }
+        }else{
+            showAddDialog(item) { name, price ->
+                val newItem = MenuItem(item.id, name, price)
+                viewModel.updateItem(isFood, newItem)
+            }
+        }
+    }
+
+    private fun showAddDialog(item: MenuItem? = null, onAdd: (String, Int) -> Unit) {
+        val dialogView = LayoutInflater.from(requireContext())
+            .inflate(R.layout.fragment_add, null)
+
+        val edtName = dialogView.findViewById<EditText>(R.id.edtName)
+        val edtPrice = dialogView.findViewById<EditText>(R.id.edtPrice)
+
+        item?.let {
+            edtName.setText(it.name)
+            edtPrice.setText(it.price.toString())
+        }
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle("Thêm món")
+            .setView(dialogView)
+            .setPositiveButton(if(item == null) "Thêm" else "Cập nhật", null)
+            .setNegativeButton("Hủy", null)
+            .create()
+
+        dialog.show()
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val name = edtName.text.toString().trim()
+            val priceText = edtPrice.text.toString().trim()
+
+            if (name.isEmpty() || priceText.isEmpty()) {
+                Toast.makeText(context, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val price = priceText.toIntOrNull()
+            if (price == null) {
+                Toast.makeText(context, "Giá không hợp lệ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            onAdd(name, price)
+            dialog.dismiss()
         }
     }
 
@@ -62,10 +136,10 @@ class ItemFragment : Fragment() {
     companion object {
         private const val ARG_TYPE = "isFood"
 
-        fun newInstance(type: Int): ItemFragment {
+        fun newInstance(type: Boolean): ItemFragment {
             val fragment = ItemFragment()
             val bundle = Bundle()
-            bundle.putInt(ARG_TYPE, type)
+            bundle.putBoolean(ARG_TYPE, type)
             fragment.arguments = bundle
             return fragment
         }
